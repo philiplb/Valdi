02b6dbcda1fbdb7d5a3e7bd54d6808be3b75501f
Philip Lehmann-Böhm <philip@philiplb.de>
2016-02-28 12:49:55 +0100

    Added a boolean validator

---
+++ tests/ValdiTests/Validator/BooleanTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace ValdiTests\Validator;
+
+use Valdi\Validator\Boolean;
+
+class BooleanTest extends \PHPUnit_Framework_TestCase {
+
+    public function testValidate() {
+        $validator = new \Valdi\Validator\Boolean();
+        $this->assertTrue($validator->validate(1, array()));
+        $this->assertTrue($validator->validate('1', array()));
+        $this->assertTrue($validator->validate(true, array()));
+        $this->assertTrue($validator->validate('on', array()));
+        $this->assertTrue($validator->validate('yes', array()));
+
+        $this->assertFalse($validator->validate(0, array()));
+        $this->assertFalse($validator->validate('0', array()));
+        $this->assertFalse($validator->validate(false, array()));
+        $this->assertFalse($validator->validate('off', array()));
+        $this->assertFalse($validator->validate('no', array()));
+
+        $this->assertFalse($validator->validate('test', array()));
+        $this->assertFalse($validator->validate('', array()));
+        $this->assertFalse($validator->validate(null, array()));
+    }
+
+}

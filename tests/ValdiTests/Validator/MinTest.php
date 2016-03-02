dad635f3b9bdcce391aa443c83505555199dba85
Philip Lehmann-Böhm <philip@philiplb.de>
2016-03-02 12:35:17 +0100

    a bit more testing for the min validator

--- tests/ValdiTests/Validator/MinTest.php
+++ tests/ValdiTests/Validator/MinTest.php
@@ -14,6 +14,11 @@ class MinTest extends \PHPUnit_Framework_TestCase {
         $this->assertTrue($validator->validate('1', array(1)));
         $this->assertTrue($validator->validate('1.1', array(1.1)));
 
+        $this->assertTrue($validator->validate(2, array(1)));
+        $this->assertTrue($validator->validate(2.1, array(1.1)));
+        $this->assertTrue($validator->validate('2', array(1)));
+        $this->assertTrue($validator->validate('2.1', array(1.1)));
+
         $this->assertFalse($validator->validate(1, array(2)));
         $this->assertFalse($validator->validate(1.1, array(2.1)));
         $this->assertFalse($validator->validate('1', array(2)));

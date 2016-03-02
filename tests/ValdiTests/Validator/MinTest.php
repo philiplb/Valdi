9988ccdde2ce86b91f34460427f6e0873cdbcf73
Philip Lehmann-Böhm <philip@philiplb.de>
2016-03-02 20:58:51 +0100

    swapped tests

--- tests/ValdiTests/Validator/MinTest.php
+++ tests/ValdiTests/Validator/MinTest.php
@@ -11,18 +11,29 @@
 
 namespace ValdiTests\Validator;
 
-use Valdi\Validator\MinLength;
+use Valdi\Validator\Min;
 
-class MinLengthTest extends \PHPUnit_Framework_TestCase {
+class MinTest extends \PHPUnit_Framework_TestCase {
 
     public function testValidate() {
-        $validator = new MinLength();
+        $validator = new Min();
 
+        $this->assertTrue($validator->validate(1, array(1)));
+        $this->assertTrue($validator->validate(1.1, array(1.1)));
         $this->assertTrue($validator->validate('1', array(1)));
-        $this->assertTrue($validator->validate('11', array(1)));
+        $this->assertTrue($validator->validate('1.1', array(1.1)));
 
+        $this->assertTrue($validator->validate(2, array(1)));
+        $this->assertTrue($validator->validate(2.1, array(1.1)));
+        $this->assertTrue($validator->validate('2', array(1)));
+        $this->assertTrue($validator->validate('2.1', array(1.1)));
+
+        $this->assertFalse($validator->validate(1, array(2)));
+        $this->assertFalse($validator->validate(1.1, array(2.1)));
         $this->assertFalse($validator->validate('1', array(2)));
-        $this->assertFalse($validator->validate('11', array(3)));
+        $this->assertFalse($validator->validate('1.1', array(2.1)));
+
+        $this->assertFalse($validator->validate('test', array(1)));
 
         $this->assertTrue($validator->validate('', array(1)));
         $this->assertTrue($validator->validate(null, array(1)));

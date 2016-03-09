ee69069a6815ba6dd50fd386391cb55a5776cd91
Philip Lehmann-Böhm <philip@philiplb.de>
2016-03-09 23:17:41 +0100

    added a "dateTimeBetween" validator

---
+++ src/Valdi/Validator/DateTimeBetween.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the Valdi package.
+ *
+ * (c) Philip Lehmann-Böhm <philip@philiplb.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Valdi\Validator;
+
+/**
+ * Validator for a date time being between the given date times.
+ * For the format, see:
+ * http://php.net/manual/en/datetime.createfromformat.php
+ */
+class DateTimeBetween extends DateTimeComparator {
+
+    /**
+     * Holds the amount of parameters.
+     */
+    protected $amountOfParameters = 2;
+
+    /**
+     * Holds the type of the validator.
+     */
+    protected $type = 'dateTimeBetween';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function compare($date, array $datetimes) {
+        return $date > $datetimes[0] && $date < $datetimes[1];
+    }
+
+}
---
+++ tests/ValdiTests/Validator/DateTimeTestBetween.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * This file is part of the Valdi package.
+ *
+ * (c) Philip Lehmann-Böhm <philip@philiplb.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ValdiTests\Validator;
+
+use Valdi\Validator\DateTimeBetween;
+
+class DateTimeBetweenTest extends \PHPUnit_Framework_TestCase {
+
+    public function testValidate() {
+        $validator = new DateTimeBetween();
+
+        $this->assertTrue($validator->validate('2016-03-29 11:23:45', array('2016-03-29 01:23:45', '2016-03-30 01:23:45')));
+        $this->assertTrue($validator->validate('20160329112345', array('2016-03-29 01:23:45', '2016-03-30 01:23:45', 'YmdHis')));
+
+        $this->assertFalse($validator->validate('2016-03-28 01:23:45', array('2016-03-29 01:23:45', '2016-03-30 01:23:45')));
+        $this->assertFalse($validator->validate('2016-03-31 01:23:45', array('2016-03-29 01:23:45', '2016-03-30 01:23:45')));
+        $this->assertFalse($validator->validate('20160328012345', array('20160329012345', '20160330012345', 'YmdHis')));
+        $this->assertFalse($validator->validate('20160330012345', array('20160329012345', '20160330012345', 'YmdHis')));
+        $this->assertFalse($validator->validate('test', array('2016-03-29 01:23:45', '2016-03-30 01:23:45')));
+
+        $this->assertTrue($validator->validate('', array('2016-03-29 01:23:45', '2016-03-30 01:23:45')));
+        $this->assertTrue($validator->validate(null, array('2016-03-29 01:23:45', '2016-03-30 01:23:45')));
+
+        try {
+            $validator->validate('2016-03-28 01:23:45', array('2016-03-29 01:23:45'));
+            $this->fail();
+        } catch (ValidationException $e) {
+            $read = $e->getMessage();
+            $expected = '"afterDateTime" expects at least 2 parameter.';
+            $this->assertSame($read, $expected);
+        } catch (Exception $e) {
+            $this->fail();
+        }
+    }
+
+}
